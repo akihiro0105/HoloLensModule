ba12fe58cae84fcbdfd747bb0fc60dff6959ba76
晃弘 上山 <akihiro@hololab.co.jp>
2018-06-04 06:08:14 +0900

    FileIO作成(中身は未実装)

---
+++ Assets/HoloLensModule/Environment/Scripts/FileIO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloLensModule.Environment
+{
+    public class FileIO 
+    {
+        // UWP Local,Document,StreamingAssets(readonly)
+        // Standalone Local,StreamingAssets(readonly)
+        // local(WWW)
+
+        public FileIO()
+        {
+
+        }
+    }
+}
